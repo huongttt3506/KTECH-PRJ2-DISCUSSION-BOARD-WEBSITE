49a4b7b7fcf5d101def5b7e2ec6461e8dbfce14f
huongttt3506 <huongttt.ueh@gmail.com>
2024-08-12 11:28:08 +0900

    first commit

---
+++ src/main/java/com/ktech_project2/discussion_board/DiscussionBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ktech_project2.discussion_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionBoardApplication.class, args);
+	}
+
+}
